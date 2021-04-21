8226ef83ba720e6c776f14627338147536a7fd52
YAN <zy980818@gmail.com>
2021-04-21 08:42:15 +0800

    Started Project

---
+++ examples.py
@@ -0,0 +1,28 @@
+message = "hello world!!"
+print(message)
+print("hello world!!")
+
+message = "hello python crash course world!"
+print(message)
+message = "Hello Python Crash Course reader!"
+print(message)
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+print("hello,"+full_name.title()+ ",!")
+print("hello,"+full_name+"!")
+message ="hello, "+full_name.title()+"!"
+print(message)
+print("\tpython")
+print("    python")
+print("Languages:\npython\nC\njavaScript\n")
+print("Languages:\n\tpython\n\tC\n\tjavaScript\n\t")
+favorite_language = 'python'
+print(favorite_language)
+favorite_language.rstrip()
+
