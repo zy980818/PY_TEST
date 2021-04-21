258f53d1f7d2b858d9b2865e382a6bb16d6c1cbc
YAN <zy980818@gmail.com>
2021-04-21 13:51:33 +0800

    third

--- apostrophe.py
+++ apostrophe.py
@@ -3,3 +3,32 @@
 # @Time    : 2021/4/21 8:59
 # @Author  : yan
 # @File    : apostrophe.py
+message = "one of Python's strengths is its diverse community"
+print(message)
+message = "one of Python's strengths is its diverse community"
+print(message)
+message = 'yan'
+print("hello "+message+",would you like to learn some Python today?")
+name = 'yan zhao'
+print("my name is "+name.lower())
+print(name.upper())
+print(name.title())
+print('Albert Einstein once said, "A person who never made a mistake nerver tried anything new."')
+name = 'Albert Einstein'
+message = '"A person who never made a mistake nerver tried anything new."'
+print(name+'once said,'+message)
+name = ' yan '
+print('\t'+name.lstrip()+'\n')
+print('\t'+name.rstrip()+'\n')
+print('\t'+name.strip()+'\n')
+age = 23
+message ="Happy " + str(age) + "rd Birthday"
+print(message)
+print(5+3)
+print(11-3)
+print(2 * 4)
+print(8/1)
+print(1)
+num = '7';
+print("my favorite number is " + str(num))
+print("my favorite number is " +num)
---
+++ bicycles.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/21 13:49
+# @Author  : yan
+# @File    : bicycles.py
