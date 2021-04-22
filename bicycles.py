0181c870fc94b57406ea303930b60708ad19625d
YAN <zy980818@gmail.com>
2021-04-22 17:14:21 +0800

    t4-22

--- bicycles.py
+++ bicycles.py
@@ -3,3 +3,11 @@
 # @Time    : 2021/4/21 13:49
 # @Author  : yan
 # @File    : bicycles.py
+bicycles = [ 'trek', 'cannodale', 'redline', 'specialized']
+print(bicycles)
+list = ['add','1',2,3]
+print(list)
+print(list[0].lower())
+print(list[-4])
+message = "my first bicycle was a " +bicycles[0].title()+"."
+print(message)
