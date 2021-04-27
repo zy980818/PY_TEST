4eb88b61dc439ef563c9de62f2f834dbb76504db
YAN <zy980818@gmail.com>
2021-04-27 09:22:35 +0800

    4-27num1

---
+++ even_numbers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/27 9:19
+# @Author  : yan
+# @File    : even_numbers.py
+even_numbers = list(range(2,11,2))
+print(even_numbers)
--- magicians.py
+++ magicians.py
@@ -7,3 +7,22 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title() + ", that was a great trick!")
     print("I can't wait to see your next trick, "+ magician.title()+ ".\n")
+print("Thank you,everyone.That was a great magic show!")
+pizzas = ['pizzaA','pizzaB','pizzaC']
+for temp in pizzas:
+    print(temp)
+for temp2 in pizzas:
+    print('I like: '+temp2+ '!')
+print('老喜欢吃披萨了，比如：'+str(pizzas)+',I really love pizza!')
+print(str(pizzas))
+animals = ['dog','cat','bird']
+for temp in animals:
+    print(temp)
+print('A '+animals[0]+' would make a great pet1')
+print('A '+animals[1]+' would make a great pet2')
+print('A '+animals[2]+' would make a great pet3')
+print('Any of these animals would make a great pet')
+for value in range(1,6):
+    print(str(value))
+number = list(range(1,6))
+print(number)
